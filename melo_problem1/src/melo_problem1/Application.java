7091f7b6efb2fbfaf7f198967c3a8fca54382efa
carlos9m9 <46663157+carlos9m9@users.noreply.github.com>
2020-03-29 21:05:41 -0400

    Problem 1 complete

--- melo_problem1/src/melo_problem1/Application.java
+++ melo_problem1/src/melo_problem1/Application.java
@@ -1,5 +1,13 @@
 package melo_problem1;
 
-public class Application {
+import java.io.IOException;
 
+public abstract class Application {
+    public static void main(String[] arg) throws IOException {
+    	
+        DuplicateRemover duplicateRemover = new DuplicateRemover();
+        duplicateRemover.remove("problem1.txt");
+        duplicateRemover.write("unique_words.txt");
+
+    }
 }
--- melo_problem1/src/melo_problem1/DuplicateRemover.java
+++ melo_problem1/src/melo_problem1/DuplicateRemover.java
@@ -1,5 +1,56 @@
 package melo_problem1;
 
-public class DuplicateRemover {
+import java.util.ArrayList;
+import java.util.List;
+import java.io.*;
 
+class DuplicateRemover {
+	
+	//Arraylist that will carry the unique words to be passed into output file
+    List<String> uniqueWords = new ArrayList<>();
+
+    public void remove(String dataFile) throws FileNotFoundException {
+    	try {
+    		//BufferReader/FileReader will allow us to read the text on the input file 
+    		BufferedReader br = new BufferedReader(new FileReader(dataFile));
+    		//Read text from file
+            String words = br.readLine();
+           
+            while (words != null) {
+            	//Create a temporary array to store the words that will be separated by the split function
+                String[] tempArr = words.split(" ");
+                
+                for (int i = 0;i < tempArr.length;i++) {
+                    if (!uniqueWords.contains(tempArr[i])) {
+                    	//The word is added if not repeated
+                        uniqueWords.add(tempArr[i]);
+                    }
+                    
+                }
+                
+                //Exit the loop
+                words = br.readLine();
+            
+            }
+            //Close buffer
+            br.close();
+            
+        }catch(IOException err) { 
+        	System.err.println("Caught IOException: " +  err.getMessage());
+        }
+    }
+
+    public void write(String outputFile) throws FileNotFoundException {
+    	try {
+    		FileWriter fWriter = new FileWriter(outputFile);
+    		//Go through the arraylist and write them into the outputfile
+	        for (int i = 0;i < uniqueWords.size();i++) {
+	        	fWriter.write(uniqueWords.get(i)+"\n");
+	        }
+	        //Close FileWriter
+	        fWriter.close();
+    	}catch(IOException err) { 
+        	System.err.println("Caught IOException: " +  err.getMessage());
+        }
+    }
 }
